c1666695f9dfa2865b1376408fd11c5d697ac835
Ahmad Jaber <ahamd.jaber010@yahoo.com>
2023-11-21 17:01:07 +0100

    fix & add new function Storie 1

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ def ls_modify_pic():
     for fichier in fichiers:
         print(fichier)
 
+def save_picture(Oject, Path, Name):
+    Oject.save(f'{Path}{Name}')
+
 # Storie 1 :
 def convert_black_and_white():
     # Demmander à l'utilisateur le nom de l'image à modifier
@@ -25,18 +28,45 @@ def convert_black_and_white():
 
     try:
         # Charger l'image
+        image = Image.open(f'{original_picture}{image_nom}')
+        if(os.path.exists(f'{modify_picture}{image_nom}')):
+            print("\nL'image existe déjà ! \n")
+        else:
+            # Convertire l'image en Noir & Blanc
+            image_noir_et_blanc = image.convert('L')
+            # image_noir_et_blanc.show()
+
+            # Sauvegarder l'image dans ./modify_picture
+            save_picture(image_noir_et_blanc, modify_picture, image_nom)
+            
+            # Retourner à l'utilisateur l'états de l'action (si err la retourné)
+            print("\nL'image a bien été transformé. \n")
+            ls_modify_pic()
+    except:
+        print("\nNous n'avons pas pu ouverire le fichier (le nom de l'image est incorrect).")
+        ls_original_pic()
+
+# convert_black_and_white()
+
+def convert_Blur():
+    # Demmander à l'utilisateur le nom de l'image à modifier
+    image_nom = input('\n Quelle est le nom de vôtre image ? : ')
+
+    try:
+        # Charger l'image
         image = Image.open(f'./original_picture/{image_nom}')
 
         # Convertire l'image en Noir & Blanc
-        image_noir_et_blanc = image.convert('L')
-        # image_noir_et_blanc.show()
+        image_blur = image.filter(ImageFilter.BLUR)
+        # image_blur.show()
 
         # Sauvegarder l'image dans ./modify_picture
-        image_noir_et_blanc.save(f'./modify_picture/{image_nom}')
+        image_blur.save(f'./modify_picture/{image_nom}')
         
         # Retourner à l'utilisateur l'états de l'action (si err la retourné)
-        print("\nL'image a bien été transformé. \n")
+        print("\nL'image a bien été transformé.")
         ls_modify_pic()
+        
     except:
         print("nous n'avons pas pu ouverire le fichier (nom d'image incorrect).")
         ls_original_pic()
