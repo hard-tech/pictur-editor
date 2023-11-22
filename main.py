0ebcaf3f05dc729cfa3007687ac264b6c320f9fd
Ahmad Jaber <ahamd.jaber010@yahoo.com>
2023-11-22 16:50:40 +0100

    Save Code

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
     original_picture = input_folder
     modify_picture = output_picture
     image_names = cat_folder_element(input_folder)
-    print(image_names)
-
     operation = filters.split('&')
-    print(f'\n{operation}')
 
     def ls_original_pic():
         fichiers = os.listdir(original_picture)
@@ -27,11 +24,11 @@ def image_transformation(filters,input_folder:str,output_picture:str):
         for fichier in fichiers:
             print(fichier)
 
-    def ls_modify_pic():
-        fichiers = os.listdir(modify_picture)
-        print(f'\nListe des fichiers dans {modify_picture} : ')
-        for fichier in fichiers:
-            print(fichier)
+    # def ls_modify_pic():
+    #     fichiers = os.listdir(modify_picture)
+    #     print(f'\nListe des fichiers dans {modify_picture} : ')
+    #     for fichier in fichiers:
+    #         print(fichier)
 
     def save_picture(object, path, name):
         object.save(f'{path}{name}')
@@ -53,7 +50,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                 original_picture = modify_picture
             else:
                 original_picture = input_folder
-
+            
             try:
                 # -- Story 1  -- #
                 if param_filter == "convert_black_and_white":
@@ -61,7 +58,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     # Charge l'image
                     image = load_picture(image_name)
 
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Convertir l'image en noir et blanc
                         image_noir_et_blanc = image.convert('L')
@@ -70,13 +67,10 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         save_picture(image_noir_et_blanc, modify_picture, image_name)
                         
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée. \n")
-                        print()
-                        ls_modify_pic()
+                        print("\nL'image a bien été transformée en noir et blanc.\n")
                     else:
                         # Retour erreur
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
 
                 # -- Story 2  -- #
                 elif param_filter == "convert_blur":
@@ -84,7 +78,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     # Charge l'image
                     image = load_picture(image_name)
                     
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Appliquer un flou à l'image
                         image_blur = image.filter(ImageFilter.BLUR)
@@ -93,12 +87,10 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         save_picture(image_blur, modify_picture, image_name)
 
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée.")
-                        ls_modify_pic()
+                        print("\nL'image a bien été floutée.\n")
                     else:
                         # Retour erreur
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
 
                 # -- Story 3  -- #
                 elif param_filter == "dilate_image":
@@ -106,7 +98,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     # Charge l'image
                     image = load_picture(image_name)
                     
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Charge l'image
                         image = cv2.imread(f'{original_picture}{image_name}')
@@ -120,12 +112,10 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         cv2.imwrite(f'{modify_picture}{image_name}', image_dilated)
 
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée.")
-                        ls_modify_pic()
+                        print("\nL'image a bien été dilater.\n")
                     else:
                         # Retour erreur
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
 
                 # -- Story 4  -- #
                 elif param_filter == f"convert_rotate:{param_filter.split(':')[1]}":
@@ -134,7 +124,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     value_rotate = param_filter.split(':')[1]
                     # Charge l'image
                     image = load_picture(image_name)
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Faire pivoter l'image
                         image_rotate = image.rotate(int(value_rotate))
@@ -143,17 +133,15 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         save_picture(image_rotate,modify_picture,image_name)
                         
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée.")
-                        ls_modify_pic()
+                        print(f"\nL'image a bien été tournée de {value_rotate}°.")
                     else:
                         # Retour erreur
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
                 
                 # -- Story 5  -- #
                 elif param_filter == f"convert_resize:{param_filter.split(':')[1]}":
                     
-                    # Demander la nouvelle taille de l'image
+                    # Récupérer la nouvelle taille de l'image
                     width_and_height = param_filter.split(':')[1]
                     value_height = int(width_and_height.split(';')[0])
                     value_width = int(width_and_height.split(';')[1])
@@ -161,7 +149,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     # Chargement l'image
                     image = cv2.imread(f'{original_picture}{image_name}')
                     
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Changer la taille de l'image
                         image_resize = cv2.resize(image, (value_width, value_height), interpolation=cv2.INTER_AREA)
@@ -170,11 +158,9 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         cv2.imwrite(f'{modify_picture}{image_name}', image_resize)
 
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée.")
-                        ls_modify_pic()
+                        print("\nL'image a bien été redimensionner.")
                     else:
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
                 
                 # -- Story 6  -- #
                 elif param_filter == f"add_text:{param_filter.split(':')[1]}":
@@ -186,7 +172,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                     # Charge l'image
                     image = cv2.imread(f'{original_picture}{image_name}')
                     
-                    # Vérifier si l'image transformée existe déjà
+                    # Vérifier si l'image s'ouvre
                     if image is not None:
                         # Obtenir les dimensions de l'image
                         image_height, image_width = image.shape[:2]
@@ -195,7 +181,7 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         font = cv2.FONT_HERSHEY_SIMPLEX
                         font_scale = 5
                         color = (255, 255, 255)  # Couleur du texte en BGR (blanc dans notre cas)
-                        thickness = 6
+                        thickness = 8
 
                         # Mesurer la taille du texte pour le centrer
                         text_size = cv2.getTextSize(text_to_add, font, font_scale, thickness)[0]
@@ -212,21 +198,17 @@ def image_transformation(filters,input_folder:str,output_picture:str):
                         cv2.imwrite(f'{modify_picture}{image_name}', image_with_text)
                         
                         # Retour l'état final à l'utilisateur
-                        print("\nL'image a bien été transformée.")
-                        ls_modify_pic()
+                        print("\nLe texte à bien été ajouter à l'image.\n")
                     else:
                         # Retour erreur
-                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.")
-                        ls_original_pic()
-                
-                elif param_filter == "help":
-                    print(f'Les fonction sont : \n--convert_black_and_white \n--convert_blur \n--dilate_image \n--convert_rotate \n--convert_resize \n--add_text')
+                        print("\nImpossible de charger l'image. Veuillez vérifier le nom du fichier.\n")
+
                 else:
                     # Retour erreur commande
                     print("\nOpération non reconnue.")
 
-            except Exception as e:
-                print(f"\nErr ({e}) --Veuillez vérifier le nom du fichier.")
+            except:
+                print(f"\nUne erreur c'est produite lors de la transformation d'un fichier.")
                 ls_original_pic()
             i+=1
 
@@ -236,6 +218,7 @@ x = 0
 input_folder=''
 filters=''
 output_folder = ''
+help = False
 
 # Récupère l'index des arguments de la commande
 for cli_name_pic in arguments:
@@ -248,5 +231,14 @@ for cli_name_pic in arguments:
     if cli_name_pic == '--o':
         output_folder = f"./{arguments[x+1]}/"
     x+=1
+    if cli_name_pic == '--help':
+        help = True
+    x+=1
 
-image_transformation(filters, input_folder, output_folder)
+if help:
+    help_msg = "\nLes fonction sont : \n\n --filters \n   'convert_black_and_white'\n   'convert_blur'\n   'dilate_image'\n   'convert_rotate'\n   'convert_resize'\n   'add_text'\n\n Selection multible avec [&] \n (exemple : 'convert_blur&convert_black_and_white')\n"\
+                "--i original_picture --o modify_picture"\
+                ""
+    print(help_msg)
+else:
+    image_transformation(filters, input_folder, output_folder)
